027dc2f4f9c7d3d235abd95f94e4c07d4e2c32ee
Erik <erickjosueuribe@gmail.com>
2024-07-20 17:20:26 -0500

    newBook done

---
+++ scripts/newBook.js
@@ -0,0 +1,49 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { BooksController } from "../controllers/books.controller.js";
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.formUser');
+    const booksController = new BooksController('http://190.147.64.47:5155');
+    form.addEventListener('submit', (event) => __awaiter(void 0, void 0, void 0, function* () {
+        event.preventDefault();
+        const title = document.getElementById('title');
+        const author = document.getElementById('author');
+        const description = document.getElementById('description');
+        const summary = document.getElementById('summary');
+        const publicationDate = document.getElementById('date');
+        const newBook = {
+            title: title.value,
+            author: author.value,
+            description: description.value,
+            summary: summary.value,
+            publicationDate: publicationDate.value
+        };
+        try {
+            // Primero, obtén el token haciendo login
+            const loginResult = yield booksController.postLogin({
+                email: 'prueba@prueba.pru',
+                password: 'C0ntr4S3gu++r4'
+            });
+            const token = loginResult.data.token;
+            // Luego, crea el libro
+            try {
+                yield booksController.createBook(newBook, token);
+                console.log('Book creation succeeded');
+                form.reset();
+            }
+            catch (error) {
+                console.log(`Error creating book: ${error}`);
+            }
+        }
+        catch (error) {
+            console.log(`Error logging in: ${error}`);
+        }
+    }));
+});
---
+++ scripts/newBook.ts
@@ -0,0 +1,46 @@
+import { BooksController } from "../controllers/books.controller.js";
+import { Book } from "../models/books.model.js";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.formUser') as HTMLFormElement;
+    const booksController = new BooksController('http://190.147.64.47:5155');
+
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const title = document.getElementById('title') as HTMLInputElement;
+        const author = document.getElementById('author') as HTMLInputElement;
+        const description = document.getElementById('description') as HTMLInputElement;
+        const summary = document.getElementById('summary') as HTMLInputElement;
+        const publicationDate = document.getElementById('date') as HTMLInputElement;
+
+        const newBook: Book = {
+            title: title.value,
+            author: author.value,
+            description: description.value,
+            summary: summary.value,
+            publicationDate: publicationDate.value 
+        };
+
+        try {
+            // Primero, obtén el token haciendo login
+            const loginResult = await booksController.postLogin({
+                email: 'prueba@prueba.pru',
+                password: 'C0ntr4S3gu++r4'
+            });
+            const token = loginResult.data.token;
+
+            // Luego, crea el libro
+            try {
+                await booksController.createBook(newBook, token);
+                console.log('Book creation succeeded');
+                form.reset();
+            } catch (error) {
+                console.log(`Error creating book: ${error}`);
+            }
+        }catch (error) {
+            console.log(`Error logging in: ${error}`);
+        }
+    });
+})
